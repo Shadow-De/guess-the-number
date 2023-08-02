e96a9cd82d7054e017a75e612aa1cfd91400ab89
Shadow DE <72759582+Shadow-De@users.noreply.github.com>
2023-08-02 11:29:44 +0530

    Add files via upload

---
+++ GUESS THE NUMBER/game1.py
@@ -0,0 +1,24 @@
+import random
+
+def guess_the_number():
+    print("Welcome to Guess the Number!")
+    secret_number = random.randint(1, 100)
+    attempts = 0
+
+    while True:
+        try:
+            guess = int(input("Enter your guess (1-100): "))
+            attempts += 1
+
+            if guess == secret_number:
+                print(f"Congratulations! You guessed the number {secret_number} in {attempts} attempts!")
+                break
+            elif guess < secret_number:
+                print("Too low! Try again.")
+            else:
+                print("Too high! Try again.")
+        except ValueError:
+            print("Invalid input! Please enter a valid number.")
+
+if __name__ == "__main__":
+    guess_the_number()
